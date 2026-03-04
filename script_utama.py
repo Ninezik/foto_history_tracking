287b9d301792f604e074efff2f80d570813e3f38
ninezik <30277780+Ninezik@users.noreply.github.com>
2026-03-04 23:48:08 +0700

    versi 2

--- script_utama.py
+++ script_utama.py
@@ -1,4 +1,8 @@
 # %%
+print('hello world')
+
+# %%
+# %%
 import json
 import requests
 import pandas as pd
@@ -8,6 +12,7 @@ import psycopg2
 # %%
 from sqlalchemy import create_engine
 
+# %%
 server = 'bansosreport-db.cmfru4yoszrg.ap-southeast-3.rds.amazonaws.com'          # atau IP server
 database = 'DB_NIPOS'
 username = 'admin'
@@ -29,22 +34,17 @@ conn = psycopg2.connect(
     port=5439
 )
 
-cursor = conn.cursor()
-cursor.execute("""SELECT connote__connote_code 
+query="""SELECT connote__connote_code,connote__created_at
             FROM nipos.nipos 
             WHERE customer_code = 'ASRBPJSKES06750A' 
             AND connote__connote_state in('DELIVERED (RETURN DELIVERY)','DELIVERED')
-            AND DATE(nipos.pod__timereceive)<current_date""")
+            AND DATE(nipos.pod__timereceive)<current_date"""
 
-rows = cursor.fetchall()
-data_set = {row[0] for row in rows}
+df_data_set=pd.read_sql(query,conn)
 
-cursor.close()
-conn.close()
-
-
+data_set= set(df_data_set['connote__connote_code'].unique())
 # %%
-query_mssql="""SELECT DISTINCT connote_code FROM connote_pod"""
+query_mssql="""SELECT DISTINCT connote_code FROM connote_pod_new"""
 
 # %%
 df_mssql=pd.read_sql(query_mssql, engine)
@@ -102,6 +102,7 @@ rows = []
 for item in all_data:
 
     connote_code = item.get("connote_code")
+    signature=item.get('pod').get('signature')
 
     for history in item.get("connote_history", []):
         if history.get("photo"):   # hanya yang ada photo
@@ -110,28 +111,30 @@ for item in all_data:
                 "connote_code": connote_code,
                 'created_at': history.get("created_at"),
                 "state": history.get("state"),
-                "photo": history.get("photo")
+                "photo": history.get("photo"),
+                'signature': signature
             })
 
 
 # %%
 import pandas as pd
 
-df = pd.DataFrame(rows)
+# df = pd.DataFrame(rows)
 
 
 # %%
-df2=pd.DataFrame()
+# df2=pd.DataFrame()
+df2=pd.DataFrame(rows)
 
 # %%
-df2=pd.concat([df2, df], ignore_index=True)
+# df2=pd.concat([df2, df], ignore_index=True)
 
 # %%
 df2.drop_duplicates(inplace=True)
 
 df2['created_at'] = pd.to_datetime(df2['created_at'])
+df2=pd.merge(df_data_set,df2,left_on='connote__connote_code', right_on='connote_code')
+df2.drop(columns=['connote__connote_code'],inplace=True)
 
 # %%
-df2.to_sql("connote_pod", engine, if_exists="append", index=False,chunksize=1000)
-
-
+df2.to_sql("connote_pod_new", engine, if_exists="append", index=False,chunksize=1000)
